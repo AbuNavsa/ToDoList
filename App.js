0c6a01e9f7382d4fb572f85511d975fd7175f123
AbuNavsa <60141291+AbuNavsa@users.noreply.github.com>
2021-06-27 03:59:59 +0100

    Added statuses to check which type of save used

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { createPortal } from 'react-dom';
 import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { colors } from './Colors';
 import Task from './Components/Task';
@@ -6,11 +7,33 @@ import Task from './Components/Task';
 export default function App() {
   const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
+  
+  {/* Status meanings = 0: Default, 1: AutoList*/}
+  let addStatus = 0;
+
+  const changeStatusAutoList = () => {
+    if (!addStatus == 1) {addStatus = 1;};
+    handleAddTask();
+  }
+
+  const changeStatusDefault = () => {
+    if (!addStatus == 0) {addStatus = 0;}
+    handleAddTask();
+  }
 
   const handleAddTask = () => {
-    Keyboard.dismiss();
-    setTaskItems([...taskItems, task])
-    setTask("");
+    if (addStatus == 0)
+      if (!task) {
+        alert("Not added");
+      }
+      else {
+        Keyboard.dismiss();
+        setTaskItems([...taskItems, task])
+        setTask("");
+      }
+    else if (addStatus == 1) {
+      alert("Auto!")
+    }
   }
 
   const completeTask = (index) => {
@@ -19,10 +42,14 @@ export default function App() {
     setTaskItems(itemsCopy);
   }
 
+  const Capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
   return (
     <View style={styles.container}>
       <View style={styles.taskWrapper}>
-        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <Text style={styles.sectionTitle}>Shopping List</Text>
 
         <View style={styles.items}>
           {/*Task items go here */}
@@ -30,7 +57,7 @@ export default function App() {
             taskItems.map((item, index) => {
               return (
               <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-                <Task text={item} />
+                <Task text={Capitalize(item)} />
               </TouchableOpacity>
 
               )
@@ -43,11 +70,23 @@ export default function App() {
       </View>
 
       <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.extraButtonsWrapper}
+      >
+        {/*Add additional buttons here*/}
+        <TouchableOpacity onPress={() => changeStatusAutoList()}>
+          <View style={[styles.addWrapper, styles.extraButtons]}>
+            <Text style={styles.addText}>=</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+      <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.writeTaskWrapper}
       >
-          <TextInput style={styles.input} placeholder={"Write a task here"} value={task} onChangeText={text => setTask(text)}/>
-          <TouchableOpacity onPress={() => handleAddTask()}>
+        <TextInput style={styles.input} placeholder={"Write a task here"} value={task} onChangeText={text => setTask(text)}/>
+          <TouchableOpacity onPress={() => changeStatusDefault()}>
             <View style={styles.addWrapper}>
               <Text style={styles.addText}>+</Text>
             </View>
@@ -70,7 +109,7 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontFamily: "Montserrat",
     fontSize: 28,
-    fontWeight: 700,
+    fontWeight: "700",
     color: colors.black
   },
   items: {
@@ -93,8 +132,7 @@ const styles = StyleSheet.create({
     borderRadius: 60,
     borderColor: "#E8E8E8",
     borderWidth: 1,
-    width: 250,
-
+    width: "75%",
   },
   addWrapper: {
     width: 55,
@@ -105,10 +143,21 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    
   },
   addText: {
     
   },
+  extraButtonsWrapper: {
+    position: 'absolute',
+    flexDirection: 'column',
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    right: '5%',
+    bottom: '25%',
+  },
+  extraButtons: {
+    marginTop: 15,
+  },
+  
 
 });
